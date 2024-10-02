27954a2d4b506ba46c353ea3df41c9a6defd05b6
xcghj1 <uphosovaumzana@gmail.com>
2024-10-02 18:51:12 +0800

    horse

---
+++ knight_move/k_move.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+
+vector<int> way(int stro, int stol, vector<vector<int>> k){
+    vector<int> str;
+
+    str.push_back(stro);
+    k.push_back();
+}
