306cc21c4b6b9738a143f378d11c7ec8d7a8511d
xcghj1 <uphosovaumzana@gmail.com>
2024-10-16 15:09:17 +0800

    Update cards.cpp

--- cards/cards.cpp
+++ cards/cards.cpp
@@ -13,32 +13,43 @@ struct card{
     char mast;
 };
 
-vector<card> res(vector<card> koloda){
-    char y = 0;
+struct findcard{
+    card desk;
+    char y;
+};
+
+findcard res(vector<card> koloda){
     int counter = 0;
+    char y = 0;
+    findcard res; 
     vector<card> twfv;
     for(int i = koloda.size()-1; i >=koloda.size()-26; i--){
         twfv.push_back(koloda[i]);
         koloda.pop_back();
     }
-    char x = koloda[koloda.size()-1].nom;
-    y = y + x;
-    koloda.pop_back();
-    for(int i = koloda.size()-1; koloda.size()-10 + x; i--){
+    while(counter!=3){
+        char x = koloda[koloda.size()-1].nom;
+        y = y + x;
         koloda.pop_back();
+        for(int i = koloda.size()-1; koloda.size()-10 + x; i--){
+            koloda.pop_back();
+        }
+        counter++;
     }
     for(int i = twfv.size()-1; i>=0; i--){
         koloda.push_back(twfv[i]);
     }
-    return koloda;
+    res.desk = koloda;
+    res.y = y;
+    return res;
 }
 
-
 int main(){
-    string cards;
+    card cards;
     vector<card> desk;
     for(int i = 0; i < 52; i++){
-        std::cin >> cards;
+        std::cin >> cards.nom;
+        std::cin >> cards.mast;
         desk.push_back(cards);
     }
     std::cout << desk[1].nom << endl;
